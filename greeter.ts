72997ccda3655426ca9a45a6c134e7a320c5f538
Tim Abell <tim@timwise.co.uk>
2020-08-11 01:16:15 +0100

    intentional type error
    Interesting that the page still loads and shows the array

--- greeter.ts
+++ greeter.ts
@@ -1,6 +1,7 @@
-function greeter(person) {
+function greeter(person: string) {
 	return "Hello, " + person;
 }
-let user = "little bobby drop tables";
+//let user = "little bobby drop tables";
+let user  = [0,1,2]; // causes type error
 
 document.body.textContent = greeter(user);
