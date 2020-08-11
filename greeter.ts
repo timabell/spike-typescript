b9e70b53f0a3816168c08ee899cb03a21f0ab1a0
Tim Abell <tim@timwise.co.uk>
2020-08-11 01:22:01 +0100

    Make an interface to pass in
    Interestingly page shows "Hello, [object Object]"
    Ts give arg error

--- greeter.ts
+++ greeter.ts
@@ -1,8 +1,14 @@
-//function greeter(person: string) {
-function greeter() {
+function greeter(person: string) {
 	return "Hello, " + person;
 }
+
+interface Person {
+	firstName: string;
+	lastName: string;
+}
+
 //let user = "little bobby drop tables";
-let user  = [0,1,2]; // causes type error
+//let user  = [0,1,2]; // causes type error
+let user = { firstName: "Bobby", lastName: "Tables" };
 
 document.body.textContent = greeter(user);
