225726e813e0c3935755c249de27cfb8ab6674d2
Tim Abell <tim@timwise.co.uk>
2020-08-11 01:28:58 +0100

    Adding a class
    Then used with an implicit interface. Nice.

--- greeter.ts
+++ greeter.ts
@@ -2,6 +2,17 @@ function greeter(person: Person) {
 	return "Hello, " + person.firstName + " " + person.lastName;
 }
 
+class Student {
+	fullName: string;
+	constructor(
+		public firstName: string,
+		public middleName: string,
+		public lastName: string,
+	) {
+		this.fullName = firstName + " " + middleName + " " + lastName;
+	}
+}
+
 interface Person {
 	firstName: string;
 	lastName: string;
@@ -9,6 +20,7 @@ interface Person {
 
 //let user = "little bobby drop tables";
 //let user  = [0,1,2]; // causes type error
-let user = { firstName: "Bobby", lastName: "Tables" };
+//let user = { firstName: "Bobby", lastName: "Tables" };
+let user = new Student ("Bobby", "D", "Tables");
 
 document.body.textContent = greeter(user);
